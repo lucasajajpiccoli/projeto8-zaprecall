072fb696663884077fade960f194d6393991bd3b
lucasajajpiccoli <lucasajaj@gmail.com>
2022-07-23 05:21:43 -0300

    Layout do mobile sem os bônus finalizado

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import "../assets/css/reset.css";
+import "../assets/css/style.css";
+
+import logo from "../assets/img/logo.png";
+import small_logo from "../assets/img/small-logo.png";
+import small_arrow from "../assets/img/small-arrow.svg";
+import party from "../assets/img/party.png";
+import sad from "../assets/img/sad.png"
+
 function App() {
     const [screen, setScreen] = React.useState(2);
 
@@ -11,19 +20,19 @@ function App() {
         <>
             {screen === 1 ? (
                 <div className="welcome" onClick={alternateScreen}>
-                    <img src="./img/logo.png" alt="logo" />
+                    <img src={logo} alt="logo" />
                     <div>ZapRecall</div>
                     <div>Iniciar Recall!</div>
                 </div>) :
                 (<div className="main" onClick={alternateScreen}>
-                    <div className="top">
-                        <img src="./img/small-logo.png" alt="logo" />
+                    <div className="header">
+                        <img src={small_logo} alt="logo" />
                         <div>ZapRecall</div>
                     </div>
                     <div clasName="questions">
                         <div className="question opened">
                             <span>Componentes devem iniciar com ...</span>
-                            <img src="./img/small-arrow.svg" alt="small-arrow" />
+                            <img src={small_arrow} alt="small-arrow" />
                         </div>
                         <div className="question answered">
                             <span>dizer para o React quais informações quando atualizadas devem renderizar a tela novamente</span>
@@ -59,8 +68,18 @@ function App() {
                         </div>
                     </div>
                 </div>)}
-            <div className="bottom">
-                
+            <div className="footer">
+                <div className="result_status">
+                    <img src={party} alt="party" />
+                    <span>Parabéns!</span>
+                </div>
+                <div className="result_message">Você não esqueceu de nenhum flashcard!</div>
+                <div className="concluded">0/8 CONCLUÍDOS</div>
+                <div className="result_icons">
+                    <ion-icon name="help-circle"></ion-icon>
+                    <ion-icon name="checkmark-circle"></ion-icon>
+                    <ion-icon name="close-circle"></ion-icon>
+                </div>
             </div>
         </>
     );
