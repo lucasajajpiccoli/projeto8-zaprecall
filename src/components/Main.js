56039c3a37ed0ac8d6f06916a450d4a1298fee33
lucasajajpiccoli <lucasajaj@gmail.com>
2022-07-25 17:36:09 -0300

    Ajuste final após esclarecimento de dúvida: armazenamento apenas via estados

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,42 +4,40 @@ import Header from "./Header";
 import Questions from "./Questions";
 import Footer from "./Footer";
 
-const deck = [{
-    question: "O que é JSX?",
-    answer: "Uma extensão de linguagem do JavaScript"
-},
-{
-    question: "O React é...",
-    answer: "uma biblioteca JavaScript para construção de interfaces"
-},
-{
-    question: "Componentes devem iniciar com...",
-    answer: "letra maiúscula"
-},
-{
-    question: "Podemos colocar ... dentro do JSX",
-    answer: "expressões"
-},
-{
-    question: "O ReactDOM nos ajuda...",
-    answer: "interagindo com a DOM para colocar componentes React na mesma"
-},
-{
-    question: "Usamos o npm para...",
-    answer: "gerenciar os pacotes necessários e suas dependências"
-},
-{
-    question: "Usamos props para...",
-    answer: "passar diferentes informações para componentes "
-},
-{
-    question: "Usamos estado (state) para...",
-    answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
-}];
-
-deck.sort(() => Math.random() - 0.5);
-
 function Main() {
+    const [deck, setDeck] = React.useState([{
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript"
+    },
+    {
+        question: "O React é...",
+        answer: "uma biblioteca JavaScript para construção de interfaces"
+    },
+    {
+        question: "Componentes devem iniciar com...",
+        answer: "letra maiúscula"
+    },
+    {
+        question: "Podemos colocar ... dentro do JSX",
+        answer: "expressões"
+    },
+    {
+        question: "O ReactDOM nos ajuda...",
+        answer: "interagindo com a DOM para colocar componentes React na mesma"
+    },
+    {
+        question: "Usamos o npm para...",
+        answer: "gerenciar os pacotes necessários e suas dependências"
+    },
+    {
+        question: "Usamos props para...",
+        answer: "passar diferentes informações para componentes "
+    },
+    {
+        question: "Usamos estado (state) para...",
+        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+    }].sort(() => Math.random() - 0.5));
+
     const [iconsAnswered, setIconsAnswered] = React.useState([]);
 
     return (
